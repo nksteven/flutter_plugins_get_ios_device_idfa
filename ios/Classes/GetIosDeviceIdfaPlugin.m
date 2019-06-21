fd2fec79fdbc7912c27eabf9d777c06c926bcdde
nksteven <yangshaoli2005@gmail.com>
2019-06-21 21:17:17 +0800

    use OC

--- ios/Classes/GetIosDeviceIdfaPlugin.m
+++ ios/Classes/GetIosDeviceIdfaPlugin.m
@@ -1,8 +1,22 @@
 #import "GetIosDeviceIdfaPlugin.h"
-#import <get_ios_device_idfa/get_ios_device_idfa-Swift.h>
+@import AdSupport;
 
 @implementation GetIosDeviceIdfaPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftGetIosDeviceIdfaPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel =
+    [FlutterMethodChannel methodChannelWithName:@"get_ios_device_idfa"
+                                binaryMessenger:[registrar messenger]];
+    GetIosDeviceIdfaPlugin* instance = [[GetIosDeviceIdfaPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if([[ASIdentifierManager sharedManager] isAdvertisingTrackingEnabled]) {
+        NSUUID *identifier = [[ASIdentifierManager sharedManager] advertisingIdentifier];
+        result([identifier UUIDString]);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
 @end
